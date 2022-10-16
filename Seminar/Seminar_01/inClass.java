bf892ee435a9dd40d2c88482eb0f0c87920a002f
Yaroslav_Bezukladnikov <ybezukladnikov@gmail.com>
2022-10-16 12:09:48 +0300

    start work

---
+++ Seminar/Seminar_01/inClass.java
@@ -0,0 +1,15 @@
+package Seminar.Seminar_01;
+
+import java.util.Scanner;
+
+/**
+ * inClass
+ */
+public class inClass {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        System.out.println(a);
+    }
+}
