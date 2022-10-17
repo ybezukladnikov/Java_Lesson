409c9177f666aa0b4b7b1a9b042adb50f675c173
Yaroslav_Bezukladnikov <ybezukladnikov@gmail.com>
2022-10-17 11:54:18 +0300

    done 3 task

---
+++ Seminar/Seminar_01/HomeWork/Task_03/task.java
@@ -0,0 +1,26 @@
+// Написать программу вычисления n-ого треугольного числа
+package Seminar.Seminar_01.HomeWork.Task_03;
+
+
+
+public class task {
+    public static void main(String[] args) {
+        System.out.println("Введите целое число: ");
+        int a = mainFunc.inputCons();
+        int result = triangle_n(a);
+        System.out.printf("Треугольное число для %s =>  %s\n", a, result);
+        
+    }
+
+    static int triangle_n(int arg) {
+        if (arg==1) {
+            return 1;
+
+            
+        } else {
+            return triangle_n(arg-1)+arg;
+            
+        }
+        
+    }
+}
